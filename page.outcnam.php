eb4319e712aea2d25733f6e09c33f60280900ab9
Franck Danard <fdanard@sangoma.com>
2022-09-20 07:34:15 +0200

    Remove open source line

--- page.outcnam.php
+++ page.outcnam.php
@@ -1,3 +1,2 @@
 <?php
 echo FreePBX::Outcnam()->showPage();
-echo '<p align="center" style="font-size:11px;">This module is maintained by the developer community at the <a target="_blank" href="http://pbxossa.org">PBX Open Source Software Alliance</a>. Support, documentation and current versions are available at the module <a target="_blank" href="https://github.com/POSSA/freepbx-Outbound_CNAM"><i class="fa fa-github"></i> Github</a>.';
